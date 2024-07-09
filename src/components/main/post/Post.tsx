cf9f565ef327004a03dade7d430d40a048662064
Abdur Rakib Rony <159608983+abdur-rakib-rony@users.noreply.github.com>
2024-07-09 17:46:22 +0600

    Update Post.tsx

--- src/components/main/post/Post.tsx
+++ src/components/main/post/Post.tsx
@@ -190,8 +190,7 @@ const Post = ({ post, currentUser }: any) => {
                 </Avatar>
                 <div className="flex-grow rounded-md bg-[#F0F2F5] p-2.5">
                   <h2 className="text-sm font-semibold text-gray-800">
-                    {currentUser.fullname ||
-                      `${comment?.userId?.firstName || ""} ${comment?.userId?.lastName || ""}`}
+                    {`${comment?.userId?.firstName || ""} ${comment?.userId?.lastName || ""}` || currentUser.fullname}
                   </h2>
                   <p className="mt-1 text-xs text-gray-600">
                     {comment.content}
