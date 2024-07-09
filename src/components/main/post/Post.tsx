78427195fd15580f5dde358d9d37c6981a93831f
Abdur Rakib Rony <159608983+abdur-rakib-rony@users.noreply.github.com>
2024-07-09 13:58:50 +0600

    Update username

--- src/components/main/post/Post.tsx
+++ src/components/main/post/Post.tsx
@@ -177,7 +177,7 @@ const Post = ({ post }: any) => {
                 </Avatar>
                 <div className="flex-grow rounded-md bg-[#F0F2F5] p-2.5">
                   <h2 className="text-sm font-semibold text-gray-800">
-                    {`${comment?.userId?.firstName && comment?.userId?.lastName ? `${comment.userId.firstName} ${comment.userId.lastName}` : "Unknown"}`}
+                   {${comment?.userId?.firstName} ${comment?.userId?.lastName}}
                   </h2>
                   <p className="mt-1 text-xs text-gray-600">
                     {comment.content}
